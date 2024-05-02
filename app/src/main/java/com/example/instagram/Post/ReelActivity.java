c25ad19ed8d8bda6a677bcb0b9e51585550b49e2
EsercitoHaki <esercitohaki2003@gmail.com>
2024-05-03 00:30:47 +0700

    activity post and reel

---
+++ app/src/main/java/com/example/instagram/Post/ReelActivity.java
@@ -0,0 +1,16 @@
+package com.example.instagram.Post;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.instagram.R;
+
+public class ReelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reel);
+    }
+}
