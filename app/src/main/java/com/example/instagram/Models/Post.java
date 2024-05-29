7e2672e259d73c44e79e6346089aa1c4e581a18e
EsercitoHaki <esercitohaki2003@gmail.com>
2024-05-29 21:16:53 +0700

    trang home

--- app/src/main/java/com/example/instagram/Models/Post.java
+++ app/src/main/java/com/example/instagram/Models/Post.java
@@ -1,7 +1,7 @@
 package com.example.instagram.Models;
 
 public class Post {
-    private String postUrl, caption;
+    private String postUrl, caption, name, time;
 
     public Post(String postUrl, String caption) {
         this.postUrl = postUrl;
@@ -10,6 +10,29 @@ public class Post {
 
     public String getPostUrl() {
         return postUrl;
+    }
+
+    public Post(String postUrl, String caption, String name, String time) {
+        this.postUrl = postUrl;
+        this.caption = caption;
+        this.name = name;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 
     public void setPostUrl(String postUrl) {
